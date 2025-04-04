4adafd92f09e944c747c5f8f6f1ad1e83e0466ce
Your Name <your.email@example.com>
2025-04-04 11:44:45 +0200

    cicd pipeline

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "ZIP the lambda functions"
 zip -j lambda/visitor_counter.zip lambda/visitor_counter.py
 zip -j lambda/reset_weekly_counter.zip lambda/reset_weekly_counter.py
+
+echo "intitialize terraform"
 cd terraform
 terraform init
+
+echo "apply changes, deploy"
 terraform apply --auto-approve
--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "visitor_counter_url" {
-  value = "${aws_apigatewayv2_api.http_api.api_endpoint}/visits"
-  description = "Public URL for the /visits endpoint"
-}
